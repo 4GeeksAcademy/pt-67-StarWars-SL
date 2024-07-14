ace240fd8d1744b485732788ba2753010539a690
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-07-14 18:40:11 +0000

    Navbarç

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,109 @@
+import { useContext } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar-light rounded-5 rounded-top border border-warning-subtle">
+			<div className="navbar">
+
+				<div className="Wars">
+					<img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX6MK34zw_YfhT1F26_4dFyF5Rc8v8_ZexPg&s" alt="Star Wars Logo" />
+				</div>
+
+				<div className="dropdown-container d-flex">
+
+
+					<div className="dropdown">
+						<a className="btn btn-warning dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorite characters ({store.charactersfavorite.length})
+						</a>
+						<ul className="dropdown-menu bg-warning">
+							{store.charactersfavorite.map((item, index) => {
+								return (
+									<li className="container-name" key={index}>
+										<Link to={`/charactersdetails/${item.id}`} className="dropdown-item bg-warning fw-bold ">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deleteCharacters(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+
+
+						<ul className="dropdown-menu bg-warning">
+							{store.vehiclesfavorite.map((item, index) => {
+								return (
+									<li key={index}>
+										<Link to={`/charactersdetails/${item.id}`} className="dropdown-item bg-warning fw-bold">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deleteCharacters(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+					</div>
+
+
+					<div className="dropdown">
+						<a className="btn btn-warning dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorite vehicles ({store.vehiclesfavorite.length})
+						</a>
+						<ul className="dropdown-menu bg-warning">
+							{store.vehiclesfavorite.map((item, index) => {
+								return (
+									<li className="container-name" key={index}>
+										<Link to={`/vehiclesdetails/${item.id}`} className="dropdown-item bg-warning fw-bold">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deleteVehicles(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+
+
+						<ul className="dropdown-menu bg-warning">
+							{store.vehiclesfavorite.map((item, index) => {
+								return (
+									<li key={index}>
+										<Link to={`/vehiclesdetails/${item.id}`} className="dropdown-item bg-warning fw-bold">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deleteVehicles(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+					</div>
+
+
+					<div className="dropdown">
+						<a className="btn btn-warning dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorite planets ({store.planetsfavorite.length})
+						</a>
+						<ul className="dropdown-menu bg-warning">
+							{store.planetsfavorite.map((item, index) => {
+								return (
+									<li className="container-name" key={index}>
+										<Link to={`/planetsdetails/${item.id}`} className="dropdown-item bg-warning fw-bold">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deletePlanets(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+
+
+						<ul className="dropdown-menu bg-warning">
+							{store.planetsfavorite.map((item, index) => {
+								return (
+									<li key={index}>
+										<Link to={`/planetsdetails/${item.id}`} className="dropdown-item bg-warning fw-bold">{item.name}</Link>
+										<button className="delete" onClick={() => actions.deletePlanets(item)}><i className="fa-solid fa-trash-can fa-2xl" ></i></button>
+									</li>
+								)
+							})}
+						</ul>
+					</div>
+
+				</div>
+
 			</div>
 		</nav>
 	);
